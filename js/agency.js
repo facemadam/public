e05595692d14ca8f53d3273089d357e036dc4faf
facemadam <tngus6047@gmail.com>
2019-03-09 13:59:02 +0900

    포트폴리오 테마 적용해보기

---
+++ js/agency.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+    // 자연스러운 스크롤 이동
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // 드랍다운 메뉴 자동 숨김
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // 스크롤 위치에 따른 메뉴 활성화
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
+    
+    // 서브메뉴 마우스 오버
+    $('.nav-item').hover(function () {
+//        $('.subbar').attr('class', 'on');
+    });
+
+})(jQuery);
